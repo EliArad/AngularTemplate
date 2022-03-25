81a83037869be9f4f1785294e6d325feb975e3f3
Eli Arad <easp13@gmail.com>
2022-03-25 09:39:51 +0300

    add clock

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  Clock: string = "00:00:00";
+  private counter: number = 0;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void
+  {
+
   }
+
+  Increase(): void
+  {
+    this.counter++;
+    let totalSeconds = this.counter;
+    let hours = Math.floor(totalSeconds / 3600);
+    totalSeconds %= 3600;
+    let minutes = Math.floor(totalSeconds / 60);
+    let seconds = totalSeconds % 60;
+    this.Clock = this.pad(hours, 2) + ":" + this.pad(minutes, 2).toString() + ":" + this.pad(seconds,2).toString();
+  }
+
+  pad(num: number, size: number): string {
+    let s = num + "";
+    while (s.length < size) s = "0" + s;
+    return s;
+  }
+
 
 }
