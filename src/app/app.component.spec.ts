0ee2bf3f28c3cf9a63107c1d6ef17701cabbe7bd
Eli Arad <easp13@gmail.com>
2022-03-27 15:53:09 +0300

    udpate to latest angular 13

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AridApp'`, () => {
+  it(`should have as title 'AngularTemplate'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AridApp');
+    expect(app.title).toEqual('AngularTemplate');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('AridApp app is running!');
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('AngularTemplate app is running!');
   });
 });
